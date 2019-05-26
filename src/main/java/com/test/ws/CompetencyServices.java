eb3412a24f57128aa85cdd12ca17a73775ef80c7
HareSRV <rvharesh00@gmail.com>
2019-05-26 16:03:58 +0530

    fetch count

--- src/main/java/com/test/ws/CompetencyServices.java
+++ src/main/java/com/test/ws/CompetencyServices.java
@@ -7,6 +7,7 @@ package com.test.ws;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.test.model.Competency;
+import com.test.model.Count;
 import java.util.List;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +34,7 @@ public class CompetencyServices {
         try {
             String sql = "select * from CMPTNCY_MAS";
             //Object[] queryParams = new Object[]{};
-            List<Competency> cmptncyList =  this.cmptncyJdbcTemplate.query(sql, new BeanPropertyRowMapper(Competency.class));
+            List<Competency> cmptncyList = this.cmptncyJdbcTemplate.query(sql, new BeanPropertyRowMapper(Competency.class));
             return new ObjectMapper().writeValueAsString(cmptncyList);
         } catch (Exception e) {            
             log.error(e.getMessage());
@@ -41,4 +42,20 @@ public class CompetencyServices {
         }
     }
     
+    
+    
+    @RequestMapping("/fetchCount")
+    public String fetchCount()  {
+        try {
+            String sql = "select CMPTNCY_CODE AS 'cmptncyCode' ,\n" +
+"CMPTNCY_LEVEL AS 'cmptncyLevel',\n" +
+"COUNT(CMPTNCY_CODE) as 'cmptncyCount' from EMP_CMPTNCY GROUP BY CMPTNCY_LEVEL,CMPTNCY_CODE;";
+            //Object[] queryParams = new Object[]{};
+            List<Count> cmptncyList = this.cmptncyJdbcTemplate.query(sql, new BeanPropertyRowMapper(Count.class));
+            return new ObjectMapper().writeValueAsString(cmptncyList);
+        } catch (Exception e) {            
+            log.error(e.getMessage());
+            return "{}";
+        }
+    }
 }
